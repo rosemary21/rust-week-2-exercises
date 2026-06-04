1321abc2308b9d5e149590d5ef0d56044e69978f
rosemary21 <chukeluchioma408@yahoo.com>
2026-06-04 22:35:36 +0100

    fix on decode_hex

--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,18 @@ pub fn decode_hex(hex_str: &str) -> Result<Vec<u8>, String> {
     let hex_str = hex_str.trim();
 
     if hex_str.len() % 2 != 0 {
-        return Err("Hex string has to be even ".to_string());
+        return Err("Hex string is not even".to_string());
     }
+
     hex_str
         .as_bytes()
         .chunks(2)
         .map(|chunk| {
             let s = std::str::from_utf8(chunk).unwrap();
-            u8::from_str_radix(s, 16).map_err(|_| format!("it is not a valid  hex byte: {}", s))
+            u8::from_str_radix(s, 16)
+                .map_err(|_| format!("it is not a valid hex byte: {}", s))
         })
-        .collect()
+        .collect::<Result<Vec<u8>, String>>() 
 }
 
 pub fn to_big_endian(bytes: &[u8]) -> Vec<u8> {
